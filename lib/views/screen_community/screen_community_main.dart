83923a5484541b00550bfdae28ccc60bac63eed0
itsMallix <amandamaulana00@gmail.com>
2023-11-29 22:52:01 +0700

    docs : add screen community

--- lib/views/screen_community/screen_community_main.dart
+++ lib/views/screen_community/screen_community_main.dart
@@ -1,13 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:freeme/component/comunity_trending.dart/community_tersimpan.dart';
+import 'package:freeme/component/comunity_trending.dart/community_trending.dart';
+import 'package:freeme/component/screen_community/community_terbary.dart';
+import 'package:freeme/component/theme/system_color.dart';
+import 'package:freeme/component/theme/system_typography.dart';
 
-class ScreenCommunityMain extends StatelessWidget {
-  const ScreenCommunityMain({super.key});
+class ScreenCommunityMain extends StatefulWidget {
+  const ScreenCommunityMain({Key? key}) : super(key: key);
+
+  @override
+  State<ScreenCommunityMain> createState() => _ScreenCommunityMainState();
+}
+
+class _ScreenCommunityMainState extends State<ScreenCommunityMain>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Community"),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: ColorSystem.primary_darkPurple,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              TabBar(
+                controller: _tabController,
+                labelStyle: TypographySystem.subtitle2,
+                labelColor: ColorSystem.primary_pastelOrange,
+                indicatorColor: ColorSystem.primary_pastelOrange,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorWeight: 2.0, // Add indicatorWeight
+                dividerColor: Colors.transparent,
+                unselectedLabelColor:
+                    ColorSystem.primary_pastelOrange.withOpacity(0.5),
+                tabs: const [
+                  Tab(text: "Trending"),
+                  Tab(text: "Terbaru"),
+                  Tab(text: "Tersimpan"),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  controller: _tabController,
+                  children: const [
+                    CommunityTrending(),
+                    CommunityTerbaru(),
+                    CommunityTersimpan(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
-    ;
   }
 }
