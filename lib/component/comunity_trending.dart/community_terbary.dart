ef543a9386bf29b9b56edbeeae0ba107ea036fb4
itsMallix <amandamaulana00@gmail.com>
2023-11-30 11:41:51 +0700

    docs : fix habits card

---
+++ lib/component/comunity_trending.dart/community_terbary.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommunityTerbaru extends StatelessWidget {
+  const CommunityTerbaru({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
