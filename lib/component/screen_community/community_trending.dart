a1e5587f4870fbf533ac31583f5ebb49a7646d9b
itsMallix <amandamaulana00@gmail.com>
2023-12-20 14:03:28 +0700

    docs : add community trending starting file

--- lib/component/screen_community/community_trending.dart
+++ lib/component/screen_community/community_trending.dart
@@ -20,7 +20,8 @@ class CommunityTrending extends StatelessWidget {
         ),
         name: "Jhon Doe",
         timestamp: "1 Jam yang lalu",
-        content: "lorem ipsum dolor sit amet",
+        content:
+            "Pada titik ini, seseorang menyadari keinginannya untuk berhenti dari kebiasaan PMO (pornografi, masturbasi, dan onani). Dengan tekad yang kuat dan kesadaran akan dampak negatifnya terhadap kesejahteraan mental dan emosional, individu ini memilih untuk mengambil langkah-langkah positif menuju pemulihan dan pertumbuhan pribadi. Melalui komitmen untuk meninggalkan kebiasaan tersebut, orang ini bertujuan untuk menciptakan kehidupan yang lebih seimbang, sehat, dan berarti.",
         likes: "56",
         comment: "12",
         share: "20",
@@ -30,9 +31,9 @@ class CommunityTrending extends StatelessWidget {
       backgroundColor: ColorSystem.primary_darkPurple,
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsetsDirectional.symmetric(horizontal: 16.0),
+          padding: const EdgeInsetsDirectional.symmetric(horizontal: 12.0),
           child: SizedBox(
-            height: 150,
+            height: 130,
             width: double.infinity,
             child: ListView.builder(
               itemCount: cards.length,
@@ -42,48 +43,60 @@ class CommunityTrending extends StatelessWidget {
               itemBuilder: (context, index) => Container(
                 padding: const EdgeInsets.only(top: 8.0),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     cards[index].profile,
                     const SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              cards[index].name,
-                              style: TypographySystem.subtitle2
-                                  .copyWith(color: ColorSystem.neutral_white),
-                            ),
-                            const SizedBox(width: 5),
-                            SizedBox(
-                              width: 20,
-                              height: 20,
-                              child: cards[index].badge,
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          cards[index].timestamp,
-                          style: TypographySystem.subtitle3.copyWith(
-                            color: ColorSystem.neutral_metallicSilver,
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.69,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                cards[index].name,
+                                style: TypographySystem.subtitle2
+                                    .copyWith(color: ColorSystem.neutral_white),
+                              ),
+                              const SizedBox(width: 5),
+                              SizedBox(
+                                width: 20,
+                                height: 20,
+                                child: cards[index].badge,
+                              ),
+                            ],
                           ),
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          cards[index].content,
-                          style: TypographySystem.caption.copyWith(
-                            color: ColorSystem.neutral_white,
-                            fontSize: 14,
+                          Text(
+                            cards[index].timestamp,
+                            style: TypographySystem.subtitle3.copyWith(
+                              color: ColorSystem.neutral_metallicSilver,
+                            ),
                           ),
-                        )
-                      ],
-                    )
+                          const SizedBox(height: 5),
+                          Text(
+                            cards[index].content,
+                            style: TypographySystem.caption.copyWith(
+                              overflow: TextOverflow.ellipsis,
+                              color: ColorSystem.neutral_white,
+                              fontSize: 14,
+                            ),
+                            maxLines: 3,
+                            textAlign: TextAlign.left,
+                          )
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.more_vert_rounded,
+                        color: ColorSystem.neutral_white,
+                      ),
+                    ),
                   ],
                 ),
               ),
