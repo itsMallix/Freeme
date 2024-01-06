ff18ad03c4384a9f39854c320cf78fd75e072c95
itsMallix <amandamaulana00@gmail.com>
2024-01-06 21:37:47 +0700

    docs : add file com tersimpan

--- lib/component/screen_community/community_tersimpan.dart
+++ lib/component/screen_community/community_tersimpan.dart
@@ -5,6 +5,10 @@ class CommunityTersimpan extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
