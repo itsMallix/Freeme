9d4b30ecacb63f37030467b762faa8e019dc0590
itsMallix <amandamaulana00@gmail.com>
2024-03-02 21:08:53 +0700

    docs : add scaffold

--- lib/component/screen_profile/card_objective.dart
+++ lib/component/screen_profile/card_objective.dart
@@ -5,6 +5,6 @@ class BuildCardObjectiveProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
